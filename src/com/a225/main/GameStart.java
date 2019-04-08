6bbd1eb7faff075cb3a3369e2798547becfdffba
weixiao1998 <1197749258@qq.com>
2019-04-08 19:53:18 +0800

    初始提交

---
+++ src/com/a225/main/GameStart.java
@@ -0,0 +1,20 @@
+package com.a225.main;
+/**
+ * 游戏启动入口
+ * @ClassName: GameStart  
+ * @Description:    
+ * @author: WeiXiao
+ * @CreateDate: 2019年4月8日 下午4:17:37
+ */
+public class GameStart {
+
+	//游戏启动入口
+	public static void main(String[] args) {
+		// 资源加载
+		// 窗体加载（自动化……）
+		// 监听加载
+		// 游戏开始
+		
+	}
+
+}
